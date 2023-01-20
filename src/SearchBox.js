55d6f9b04065391d9424d2ed5d90dbdeb0b95a36
7vault <wnestand.dev@gmail.com>
2023-01-20 00:34:11 -0800

    searchbox implemented

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -3,7 +3,7 @@ import SearchIcon from "./search.svg";
 
 const SearchBox = ({ onSearchChange }) => {
   return (
-    <div className=" tc search">
+    <div className="search">
       <input placeholder="Search for movies" onChange={onSearchChange} />
       <img src={SearchIcon} alt="search" onClick={() => {}}></img>
     </div>
